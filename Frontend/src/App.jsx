a66f34089549da93988b669a9dad184f7154e309
MUHAMMAD ANAS <154446804+anashanif123@users.noreply.github.com>
2025-09-01 23:31:44 +0500

    add vercel analytics

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,17 +1,20 @@
 import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react"; 
 import Landing from "./pages/Landing";
 import ResultPage from "./pages/ResultPage";
 import AdminLogin from "./pages/AdminLogin";
 import AdminDashboard from "./pages/AdminDashboard";
+
 export default function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Landing />} />
-      <Route path="/result" element={<ResultPage />} />
-      <Route path="/admin" element={<AdminLogin />} />
-      <Route path="/dashboard" element={<AdminDashboard />} />
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/result" element={<ResultPage />} />
+        <Route path="/admin" element={<AdminLogin />} />
+        <Route path="/dashboard" element={<AdminDashboard />} />
+      </Routes>
+      <Analytics /> 
     </BrowserRouter>
   );
 }
